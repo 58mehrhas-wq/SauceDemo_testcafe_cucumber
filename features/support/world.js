380ca7ebbdb89500af77d18333e3a498a5674404
58mehrhas-wq <63117899+58mehrhas-wq@users.noreply.github.com>
2026-07-27 20:39:06 -0700

    Changed to Chrome

--- features/support/world.js
+++ features/support/world.js
@@ -13,7 +13,7 @@ function CustomWorld({attach, parameters}) {
 
     this.setBrowser = function() {
         if (parameters.browser === undefined) {
-            return 'edge';
+            return 'chrome';
         } else {
             return parameters.browser;
         }
